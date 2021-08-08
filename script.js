10cc2a3c302ff02f5df7a1ca4b4d5f4f58e874a5
Mazin Ashfaq <mzash00@gmail.com>
2021-08-08 12:55:57 -0700

    added temp backround and overlay

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-particlesJS.load("particles-js", "particles.json", function () {
-  console.log("callback - particles.js config loaded");
-});
-
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
