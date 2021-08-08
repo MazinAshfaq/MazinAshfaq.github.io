bb488e724f8674300cdcaab1e75cb75ba6b4447f
Mazin Ashfaq <mzash00@gmail.com>
2021-08-07 22:34:34 -0700

    some changes

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+particlesJS.load("particles-js", "particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
