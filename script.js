f37beaebe6b5cd9ca514add3fb1fad40e9faa95a
Mazin Ashfaq <mzash00@gmail.com>
2022-06-06 12:06:56 -0700

    added skills section and smooth scroll, working on project section

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-aa;
+/* Smooth Scrolling */
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
