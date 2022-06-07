5dc4a3e2a14885f6cacac3cd339972b4f0795d84
Mazin Ashfaq <mzash00@gmail.com>
2022-06-06 18:23:22 -0700

    made some style changes

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+/*Sticky Navbar*/
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
